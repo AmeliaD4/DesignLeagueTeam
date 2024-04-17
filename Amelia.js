eae6e82f73a02fd2016878e8b08fc7da9369570c
A.D <ameliad15@nycstudents.net>
2024-04-17 12:07:52 -0400

    Update Amelia.js

--- Amelia.js
+++ Amelia.js
@@ -1,13 +1,13 @@
 //Loader
-window.addEventListener('load',()=> {
+window.addEventListener('load', () => {
   const loader = document.querySelector('.loader');
   setTimeout(() => {
     loader.setAttribute('style', 'opacity:0;');
   }, 500);
   setTimeout(() => {
     loader.patentNode.removeChild(loader);
-    document.body.style.overflow = 'initial;
-  }, 1000)
+    document.body.style.overflow = 'initial';
+  }, 1000);
 });
 
 
