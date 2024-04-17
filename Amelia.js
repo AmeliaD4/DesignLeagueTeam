a06246801d7c3fd991251103eed61918cab6ec63
A.D <ameliad15@nycstudents.net>
2024-04-17 12:06:36 -0400

    Create Amelia.js

---
+++ Amelia.js
@@ -0,0 +1,28 @@
+//Loader
+window.addEventListener('load',()=> {
+  const loader = document.querySelector('.loader');
+  setTimeout(() => {
+    loader.setAttribute('style', 'opacity:0;');
+  }, 500);
+  setTimeout(() => {
+    loader.patentNode.removeChild(loader);
+    document.body.style.overflow = 'initial;
+  }, 1000)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
